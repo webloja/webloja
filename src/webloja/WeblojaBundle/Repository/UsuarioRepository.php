5d1f52375480a5b130a5b32cb85730a997a2bf29
douglas silva <douglas@dsanalista.com>
2013-04-25 17:14:38 -0300

    Alterado para recuperar a descrição do perfil

--- src/webloja/WeblojaBundle/Repository/UsuarioRepository.php
+++ src/webloja/WeblojaBundle/Repository/UsuarioRepository.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityRepository;
 class UsuarioRepository extends EntityRepository {
     
     public function getUsuarioSenha($login,$senha){
+
         $ativo = 1;
         $query = $this->getEntityManager()
                 ->createQuery('SELECT u FROM WeblojaBundle:Usuario u WHERE u.login = :login and u.senha = :senha and u.ativo = :ativo')
@@ -15,6 +16,16 @@ class UsuarioRepository extends EntityRepository {
 
         return $query->getSingleResult();
       
+    }
+    
+    public function getPerfil($id){
+        
+        $query = $this->getEntityManager()
+                ->createQuery('SELECT p FROM WeblojaBundle:Perfil p WHERE p.id = :id')
+                ->setParameter('id',$id);
+
+        return $query->getSingleResult();
+        
     }
     
 }
